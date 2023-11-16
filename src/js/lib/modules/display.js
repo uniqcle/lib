de13ddadfffc628f111d80390dcc9714412e503d
uniqcle <uniqcle@yandex.ru>
2023-11-16 14:28:40 +0700

    hide method

--- src/js/lib/modules/display.js
+++ src/js/lib/modules/display.js
@@ -9,3 +9,13 @@ $.prototype.show = function () {
   }
   return this; 
 };
+
+$.prototype.hide = function () {
+  for (let i = 0; i < this.length; i++) {
+    if (!this[i].style) {
+      continue;
+    }
+    this[i].style.display = "none";
+  }
+  return hide;
+};
